2f710cff8d12c7b2a2a051fc64a1882cadd2db48
Boshra Jaber <boshrajaber94@gmail.com>
2021-03-17 11:44:25 +0200

    backend done

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const superAgent = require('superagent');
 const pg = require('pg');
 const cors = require('cors');
 const methodOverride = require('method-override');
+const { redirect } = require('statuses');
 
 // setup and configuration
 require('dotenv').config();
@@ -19,8 +20,12 @@ const client = new pg.Client(process.env.DATABASE_URL);   // on your machine
 PORT=process.env.PORT;
 //routes
 app.get('/',handleHomePage);
-app.post('/getCountryResult', handleCountryResult)
-app.get('/AllCountries', handleAllCountriesData)
+app.post('/getCountryResult', handleCountryResult);
+app.get('/AllCountries', handleAllCountriesData);
+app.post('/MyRecords',handleRecords);
+app.get('/MyRecords',handleRecordsDisplay);
+app.post('/RecordDetails/:id',handleRecordDetails)
+app.delete('/MyRecords/:id',handleDelete);
 
 //handlers
 function handleHomePage(req,res){
@@ -53,7 +58,38 @@ function handleAllCountriesData(req,res){
         res.render('AllCountries',{result: countryArr})
     })
 }
-
+function handleRecords(req,res){
+    let data= req.body;
+    let sql= 'INSERT INTO country (country,totalConfirmedCases,totalDeathsCases,totalRecoveredCases,date) VALUES ($1,$2,$3, $4, $5) RETURNING *;'
+    let safeValues = [data.country, data.totalConfirmedCases, data.totalDeathsCases , data.totalRecoveredCases , data.date];
+    client.query(sql,safeValues).then(data=>{
+        res.redirect('/MyRecords');
+    //    console.log(data.rows);
+    })
+}
+function handleRecordsDisplay(req,res){
+    let sql='SELECT * FROM country;'
+    client.query(sql).then(data=>{
+        res.render('records', {result: data.rows})
+    })
+}
+function handleRecordDetails(req,res){
+    let id= req.params.id;
+    let sql='SELECT * FROM country WHERE id=$1;'
+    let safeValue=[id];
+    client.query(sql,safeValue).then(data=>{
+        console.log( data.rows[0]);
+        res.render('details', {result: data.rows[0]})
+    })
+}
+function handleDelete(req,res){
+    let id=req.params.id;
+    let sql='DELETE FROM country WHERE id=$1;'
+    let safeValue=[id];
+    client.query(sql,safeValue).then(data=>{
+        res.redirect('/MyRecords')
+    })
+}
 //constructor
 function Country(data){
     this.country = data.Country
